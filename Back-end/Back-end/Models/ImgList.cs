f189c47231138d75d8ef78b6ea1e1cbfc3cb7404
Vũ Huy Hoàng <vhhoang0120@gmail.com>
2023-11-23 20:26:28 +0700

    basic implement of models

---
+++ Back-end/Back-end/Models/ImgList.cs
@@ -0,0 +1,9 @@
+﻿namespace Back_end.Models
+{
+    public class ImgList
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Type { get; set; }
+    }
+}
