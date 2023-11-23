b124d3a725f4f178377f753493a1626b5bebaf54
Vũ Huy Hoàng <vhhoang0120@gmail.com>
2023-11-23 20:26:28 +0700

    basic implement of models

---
+++ Back-end/Back-end/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿namespace Back_end.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string RefreshToken { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        // AreaManaged??
+    }
+}
---
+++ Back-end/Back-end/Models/AdsPoint.cs
@@ -0,0 +1,14 @@
+﻿namespace Back_end.Models
+{
+    public class AdsPoint
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Address { get; set; }
+        public string LocationType { get; set; }
+        public string AdsForm { get; set; }
+        public bool Planned { get; set; }
+        // Image??
+        // AdsList??
+    }
+}
