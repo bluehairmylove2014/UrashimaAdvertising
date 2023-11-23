866ecf2a226c240ab79c014f04476a4df8e8bb52
Vũ Huy Hoàng <vhhoang0120@gmail.com>
2023-11-23 20:26:28 +0700

    basic implement of models

---
+++ Back-end/Back-end/Models/AdsModifyRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Back_end.Models
+{
+    public class AdsModifyRequest
+    {
+        public string Reasons { get; set; }
+        public DateTime ModifyTime { get; set; }
+    }
+}
---
+++ Back-end/Back-end/Models/Report.cs
@@ -0,0 +1,14 @@
+﻿namespace Back_end.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public string ReportType { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Content { get; set; }
+        public string ReportStatus { get; set; }
+        // Image??
+    }
+}
