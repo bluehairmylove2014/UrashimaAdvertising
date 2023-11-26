48eaf532af7251a8cdcddc0be1362dd913fc713f
cuong457 <pnccuong20@clc.fitus.edu.vn>
2023-11-26 15:28:06 +0700

    Rename server, add basic model

--- Back-end/Back-end/Models/ImgList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Back_end.Models
-{
-    public class ImgList
-    {
-        public int Id { get; set; }
-        public string Image { get; set; }
-        public string Type { get; set; }
-    }
-}
