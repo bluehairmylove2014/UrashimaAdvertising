6701c6ae9de4cd95d196619c428336f8932dd00e
cuong457 <pnccuong20@clc.fitus.edu.vn>
2023-11-26 15:28:06 +0700

    Rename server, add basic model

--- Back-end/Back-end/Models/AdsModifyRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Back_end.Models
-{
-    public class AdsModifyRequest
-    {
-        public string Reasons { get; set; }
-        public DateTime ModifyTime { get; set; }
-    }
-}
---
+++ UrashimaServer/UrashimaServer/Database/Models/Report.cs
@@ -0,0 +1,14 @@
+﻿namespace UrashimaServer.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public string ReportType { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string ReportStatus { get; set; } = string.Empty;
+        public string TreatmentProcess { get; set; } = string.Empty;
+    }
+}
