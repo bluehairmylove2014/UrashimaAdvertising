b7d62a183310d8102eb9c463667122f20d479a07
cuong457 <pnccuong20@clc.fitus.edu.vn>
2023-11-26 15:28:06 +0700

    Rename server, add basic model

---
+++ UrashimaServer/UrashimaServer/Database/Models/AdsBoard.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrashimaServer.Models
+{
+    public class AdsBoard
+    {
+        [Key]
+        public int Id { get; set; }
+        [Key]
+        public int AdsPointId { get; set; }
+        public string AdsType { get; set; } = string.Empty;
+        public int Size { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public DateTime ExpiredDate { get; set; }
+    }
+}
