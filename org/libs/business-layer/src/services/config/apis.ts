fc7d2ff6cab7a7dead9ff683816ca6ab306f3901
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2023-11-27 22:50:46 +0700

    UPDATE/GeolocateControl&FullscreenControl&NavigationControl&ScaleControl

--- org/libs/business-layer/src/services/config/apis.ts
+++ org/libs/business-layer/src/services/config/apis.ts
@@ -1,5 +1,8 @@
 // PROXY_URL
-const proxyUrl = 'https://ura-ads.phucdat4102.workers.dev/cors-proxy/';
+const workerUrl = 'https://ura-ads.phucdat4102.workers.dev';
+// const ipLocationProxyUrl = workerUrl + '/ip-location' + `?ipaddress=`;
+const ipLocationProxyUrl = 'http://ip-api.com/json';
+const corsProxyUrl = workerUrl + '/cors-proxy/';
 // 'https://cors-anywhere.herokuapp.com/';
 
 // AUTH
@@ -10,20 +13,25 @@ export const updateAccountUrl = '/v1/auth/login-social';
 // ADS
 export const getAllAdsUrl = '/api/ads-points';
 
+// IP LOCATION
+export const getCurrentLocationUrl = ipLocationProxyUrl;
+
 // GOOGLE
 export const googleGetUserInfoUrl =
-  proxyUrl +
+  corsProxyUrl +
   'https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses';
 export const googleValidateTokenUrl =
-  proxyUrl + 'https://www.googleapis.com/oauth2/v3/tokeninfo';
+  corsProxyUrl + 'https://www.googleapis.com/oauth2/v3/tokeninfo';
 // FACEBOOK
 export const facebookGetFBAccessTokenUrl =
-  proxyUrl + 'https://graph.facebook.com/v17.0/oauth/access_token';
+  corsProxyUrl + 'https://graph.facebook.com/v17.0/oauth/access_token';
 export const facebookGetFBUserInfoUrl =
-  proxyUrl + 'https://graph.facebook.com/me?fields=first_name,last_name,email';
+  corsProxyUrl +
+  'https://graph.facebook.com/me?fields=first_name,last_name,email';
 // GITHUB
 export const githubGetAccessTokenUrl =
-  proxyUrl + 'https://github.com/login/oauth/access_token';
+  corsProxyUrl + 'https://github.com/login/oauth/access_token';
 export const githubValidateTokenUrl =
-  proxyUrl + 'https://api.github.com/authorizations/';
-export const githubGetUserInfoUrl = proxyUrl + 'https://api.github.com/user';
+  corsProxyUrl + 'https://api.github.com/authorizations/';
+export const githubGetUserInfoUrl =
+  corsProxyUrl + 'https://api.github.com/user';
--- org/libs/business-layer/src/services/lib/index.ts
+++ org/libs/business-layer/src/services/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './authService';
 export * from './socialService';
+export * from './ipService';
