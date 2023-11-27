bcad91475d163380cd3a485d9779fdccbfbf4ceb
lyvandat <75932518+lyvandat@users.noreply.github.com>
2023-11-27 20:51:34 +0700

    add git ignore for backend

---
+++ Back-end/Back-end/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            return Ok();
+        }
+    }
+}
