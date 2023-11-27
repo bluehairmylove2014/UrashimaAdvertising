41a2b7a4f4458e7f70e3a6762da5c2a1cbcb2351
lyvandat <75932518+lyvandat@users.noreply.github.com>
2023-11-27 22:44:49 +0700

    Add auto mapper

---
+++ UrashimaServer/UrashimaServer/Database/Dtos/GetBoardDto.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+
+namespace UrashimaServer.Models
+{
+    public class GetAdsBoardDto
+    {
+        public int Id { get; set; }
+        public int AdsPointId { get; set; }
+        public string AdsType { get; set; } = string.Empty;
+        public int Size { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public DateTime ExpiredDate { get; set; }
+
+        public GetAdsPointDto? AdsPoint { set; get; }
+        public ICollection<Report>? Reports { set; get; }
+    }
+
+    public class GetAdsPointDto
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string LocationType { get; set; } = string.Empty;
+        public string AdsForm { get; set; } = string.Empty;
+        public bool Planned { get; set; }
+        public ICollection<AdsPointImage>? Images { get; set; }
+    }
+}
--- UrashimaServer/UrashimaServer/Database/Models/Account.cs
+++ UrashimaServer/UrashimaServer/Database/Models/Account.cs
@@ -13,4 +13,13 @@ namespace UrashimaServer.Models
         public string Role { get; set; } = string.Empty;
         public string AreaManaged { get; set; } = string.Empty; // List - modify later
     }
+
+    public class AccountDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string PasswordSalt { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
 }
