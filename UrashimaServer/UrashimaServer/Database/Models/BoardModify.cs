98295ae30cb845fca872addd1bba318941993801
Vũ Huy Hoàng <vhhoang0120@gmail.com>
2023-12-05 21:29:28 +0700

    Adsboard, adpoint, model for officer

---
+++ UrashimaServer/UrashimaServer/Database/Models/BoardModify.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrashimaServer.Database.Models
+{
+    public class BoardModify
+    {
+        [Key]
+        public int Id { get; set; }
+        public int BoardID { get; set; }
+        public int AdsPointId { get; set; }
+        public string AdsType { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Image {  get; set; }
+        public DateTime ExpiredDate { get; set; }
+    }
+}
