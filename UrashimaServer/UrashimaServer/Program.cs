a3eec873719a9c9004a685b192c32bb200709a32
lyvandat <75932518+lyvandat@users.noreply.github.com>
2023-12-17 22:21:27 +0700

    add port 2808 and 2816

--- UrashimaServer/UrashimaServer/Program.cs
+++ UrashimaServer/UrashimaServer/Program.cs
@@ -44,7 +44,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
     policy =>
     {
+        policy.WithOrigins("http://localhost").AllowAnyMethod().AllowAnyHeader();
         policy.WithOrigins("http://localhost:*").AllowAnyMethod().AllowAnyHeader();
+        policy.WithOrigins("http://localhost:2808").AllowAnyMethod().AllowAnyHeader();
+        policy.WithOrigins("http://localhost:2816").AllowAnyMethod().AllowAnyHeader();
     }));
 // Auto Mapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
