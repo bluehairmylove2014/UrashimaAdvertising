b6869cf65d2fa2d36f15d20a5d719c364049aa60
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2023-12-25 13:37:15 +0700

    UPDATE/ Renew officer report page

--- org/apps/officer-ua/app/(main)/reports/page.tsx
+++ org/apps/officer-ua/app/(main)/reports/page.tsx
@@ -1,24 +1,4 @@
-import { COOKIE_KEYS } from '@business-layer/business-logic/configs/constants';
-import { ReportService } from '@business-layer/services';
-import { cookies } from 'next/headers';
-
-const officerService = new ReportService();
-async function getAdLocationDetail() {
-  try {
-    const token = cookies().get(COOKIE_KEYS.ACCESS_TOKEN)?.value ?? null;
-    if (token) {
-      return await officerService.getAllOfficerReport(token);
-    } else {
-      return null;
-    }
-  } catch (error) {
-    return null;
-  }
-}
-
-async function Report() {
-  const adData = await getAdLocationDetail();
-  console.log(adData);
+function Report() {
   return <div>Report Page</div>;
 }
 
