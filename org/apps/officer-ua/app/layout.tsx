ae3d2ac83081b776bc4dd9f4827a046a64238b39
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2023-12-31 16:35:03 +0700

    ADD/CSS Flaticon

--- org/apps/officer-ua/app/layout.tsx
+++ org/apps/officer-ua/app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.scss';
+import '@flaticon/flaticon-uicons/css/all';
 import { Montserrat } from 'next/font/google';
 import { BusinessLogicProvider } from '@business-layer/business-logic/provider';
 import Notification from '@presentational/atoms/Notification';
