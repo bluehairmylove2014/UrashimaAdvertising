831ba63027b333fff144bdcb2c66d86d270c8bcd
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2024-01-01 13:24:57 +0700

    New Deployment

--- org/libs/business-layer/src/services/lib/adsService/index.ts
+++ org/libs/business-layer/src/services/lib/adsService/index.ts
@@ -92,7 +92,6 @@ export class AdsService extends Services {
         throw new Error('Unauthorized');
       }
     } catch (error) {
-      console.log('CALL GET ERIOR: ', error);
       throw this.handleError(error);
     }
   };
